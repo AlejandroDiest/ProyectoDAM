061afbb560574ac2596e386ab93e2d2b44dd87d0
AlexAnti1 <dragut.andav24@zaragoza.salesianos.edu>
2025-04-07 15:57:04 +0200

    Primer commit del proyecto

---
+++ src/main/java/com/servlet/register/RegisterServlet.java
@@ -0,0 +1,82 @@
+/**
+*Autor:David Alexandru Dragut Anti Martinica
+*Fecha: 31 mar 2025
++Práctica
+*Descripción:
+*/
+package com.servlet.register;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+	// create the query
+	private static final String INSERT_QUERY = "INSERT INTO USERs(NAME,CITY,MOBILE,DOB) VALUES(?,?,?,?)";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// get PrintWriter, para imprimir errores en los catch
+		PrintWriter pw = res.getWriter();
+		// Establecemos el tipo de contenido
+		res.setContentType("text/hmtl");
+		// Leemos los valores del formulario
+		String name = req.getParameter("name");
+		String city = req.getParameter("city");
+		String mobile = req.getParameter("mobile");
+		String dob = req.getParameter("dob");
+
+		// load the jdbc driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// create the connection
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/prueba?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+				"root", "3032005ALEXANTI"); PreparedStatement ps = con.prepareStatement(INSERT_QUERY);) {
+			// set the values
+			ps.setString(1, name);
+			ps.setString(2, city);
+			ps.setString(3, mobile);
+			ps.setString(4, dob);
+
+			// execute the query
+			int count = ps.executeUpdate();
+
+			if (count == 0) {
+				pw.println("Record not stored into database");
+			} else {
+				pw.println("Record Stored into Database");
+			}
+		} catch (SQLException se) {
+			pw.println(se.getMessage());
+			se.printStackTrace();
+		} catch (Exception e) {
+			pw.println(e.getMessage());
+			e.printStackTrace();
+		}
+		// close the stream
+		pw.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+}
